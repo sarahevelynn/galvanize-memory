d3298e283f63e834b98fabca8f3a3ee01605b18a
SarahEvelynn Johnson <sarahevelynn@SarahEvelynns-MacBook-Pro.local>
2018-01-12 13:43:12 -0700

    finished drill 3

---
+++ drills/version-3/migrations/20180112131258_games.js
@@ -0,0 +1,13 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable("games", table => {
+    table.increments("id").primary();
+    table.text("name");
+    table.text("developer");
+    table.float("rating");
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTableIfExists("games");
+};
